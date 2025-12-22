6003ee7880ac185e548a7f36c4b2b72d8b441f55
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-12-22 15:45:39 -0800

    section 7 complete

---
+++ Methods/MethodsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,147 @@
+﻿namespace ClassLibrary1;
+
+public class Product
+{
+    //fields
+    private int productID;
+    private string productName;
+    private double cost;
+    public double tax;
+    private int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Electronics";
+    private readonly string dateOfPurchase;
+
+    //constructor
+    public Product()
+    {
+        dateOfPurchase = DateTime.Now.ToShortDateString();
+    }
+
+    //Set method for productID
+    public void SetProductID(int productID)
+    {
+        this.productID = productID;
+    }
+
+    //Get method for productID
+    public int GetProductID()
+    {
+        return productID;
+    }
+
+    //Set method for productName
+    public void SetProductName(string value)
+    {
+        productName = value;
+    }
+
+    //Get method for productID
+    public string GetProductName()
+    {
+        return productName;
+    }
+
+    //Set method for cost
+    public void SetCost(double value)
+    {
+        cost = value;
+    }
+
+    //Get method for cost
+    public double GetCost()
+    {
+        return cost;
+    }
+
+    //Set method for tax
+    public void SetTax(double value)
+    {
+        tax = value;
+    }
+
+    //get method for tax
+    public double GetTax()
+    {
+        return tax;
+    }
+
+    //method
+    /*
+        cost <= 20000 then tax = 10%
+        cost > 20000 then tax = 12.5%
+    */
+    public void CalculateTax(double percentage)
+    {
+        percentage = 10.45;
+        //create local variable
+        double t;
+
+        //calculate tax
+        if (this.cost <= 20000)
+        {
+            t = cost * 10 / 100;
+        }
+        else
+        {
+            t = cost * percentage / 100;
+        }
+        tax = t;
+    }
+
+    //method overloading
+    public void CalculateTax(double cost, double percentage)
+    {
+        //create local variable
+        double t;
+
+        //calculate tax
+        if (this.cost <= 50000)
+        {
+            t = cost * 5 / 100;
+        }
+        else
+        {
+            t = cost * percentage / 100;
+        }
+        tax = t;
+    }
+
+    //set method for quantityInStock
+    public void SetQuantityInStock(int value)
+    {
+        quantityInStock = value;
+    }
+
+    //get method for quantityInStock
+    public int GetQuantityInStock()
+    {
+        return quantityInStock;
+    }
+
+    //get method for dateOfPurchase
+    public string GetDateOfPurchase()
+    {
+        return dateOfPurchase;
+    }
+
+    //static method: Set method TotalNoOfProducts
+    public static void SetTotalNoOfProducts(int value)
+    {
+        TotalNoProducts = value;
+    }
+
+    //static method: Set method TotalNoOfProducts
+    public static int GetTotalNoOfProducts()
+    {
+        return TotalNoProducts;
+    }
+
+    //static method: Calculates Total Quantity
+    public static int GetTotalQuantity(Product product1, Product product2, Product product3)
+    {
+        int total;
+        total = product1.GetQuantityInStock() + product2.GetQuantityInStock() + product3.GetQuantityInStock();
+        return total;
+    }
+}
---
+++ Methods/MethodsExample/MethodsExample/Program.cs
@@ -0,0 +1,80 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+        //local constant
+        const string developerName = "Jesse";
+
+        //create reference variables
+        Product product1, product2, product3;
+
+        //create objects
+        product1 = new Product();
+        Product.SetTotalNoOfProducts(Product.GetTotalNoOfProducts() + 1); //1
+        product2 = new Product();
+        Product.SetTotalNoOfProducts(Product.GetTotalNoOfProducts() + 1); //2
+        product3 = new Product();
+        Product.SetTotalNoOfProducts(Product.GetTotalNoOfProducts() + 1); //3
+
+        //initalize fields
+        product1.SetProductID(1001);
+        product1.SetProductName("Mobile");
+        product1.SetCost(20000);
+        product1.SetQuantityInStock(1200);
+
+        product2.SetProductID(1003);
+        product2.SetProductName("Laptop");
+        product2.SetCost(45000);
+        product2.SetQuantityInStock(3400);
+
+        product3.SetProductID(1003);
+        product3.SetProductName("Speakers");
+        product3.SetCost(36000);
+        product3.SetQuantityInStock(800);
+        
+        //call methods
+        //named arguments percentage: 9.2
+        product1.CalculateTax(percentage: 9.2);
+        double p = 7.4;
+        product2.CalculateTax(p);
+        Console.WriteLine("p is " + p);
+        product3.CalculateTax(10000, 3.4);
+
+        //get values from fields
+        Console.WriteLine(developerName);
+
+        Console.WriteLine("Product 1:");
+        Console.WriteLine("Product ID: " + product1.GetProductID());
+        Console.WriteLine("Product Name: " + product1.GetProductName());
+        Console.WriteLine("Cost: " + product1.GetCost());
+        Console.WriteLine("Quantity in Stock: " + product1.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product1.GetDateOfPurchase());
+        Console.WriteLine("Tax: " + product1.GetTax());
+
+        Console.WriteLine("\nProduct 2:");
+        Console.WriteLine("Product ID: " + product2.GetProductID());
+        Console.WriteLine("Product Name: " + product2.GetProductName());
+        Console.WriteLine("Cost: " + product2.GetCost());
+        Console.WriteLine("Quantity in Stock: " + product2.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product2.GetDateOfPurchase());
+        Console.WriteLine("Tax: " + product2.GetTax());
+
+        Console.WriteLine("\nProduct 3:");
+        Console.WriteLine("Product ID: " + product3.GetProductID());
+        Console.WriteLine("Product Name: " + product3.GetProductName());
+        Console.WriteLine("Cost: " + product3.GetCost());
+        Console.WriteLine("Quantity in Stock: " + product3.GetQuantityInStock());
+        Console.WriteLine("Date of Purchase: " + product3.GetDateOfPurchase());
+        Console.WriteLine("Tax: " + product3.GetTax());
+
+        //total quantity
+        int totalQuantity = Product.GetTotalQuantity(product1, product2, product3);
+
+        //display totals
+        Console.WriteLine("Total Quantity: " + totalQuantity);
+        Console.WriteLine("Total no. of products: " + Product.GetTotalNoOfProducts()); //Output: 3
+        Console.WriteLine("Category of Products: " + Product.CategoryName);
+    }
+}
