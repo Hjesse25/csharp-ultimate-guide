d6f1ace8611e323bb34955d08dbb74c4d433c4b5
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-12-24 08:29:16 -0800

    section 8 complete

---
+++ Constructors/ConstructorsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿public class Employee
+{
+    //fields
+    public int empID;
+    public string empName;
+    public string job;
+
+    //static field
+    public static string companyName;
+
+    //instance constructor
+    public Employee(int empID, string empName, string job)
+    {
+        this.empID = empID;
+        this.empName = empName;
+        this.job = job;
+    }
+
+    public Employee(int empID, string empName)
+    {
+        this.empID = empID;
+        this.empName = empName;
+    }
+
+    public Employee()
+    {
+        empID = 1;
+    }
+
+    //static constructor
+    static Employee()
+    {
+        companyName = "ABC Industries";
+    }
+}
---
+++ Constructors/ConstructorsExample/ConstructorsExample/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //create 3 objects for Employee
+        Employee emp1 = new Employee(); //constructor 3
+        emp1.empID = 101;
+        emp1.empName = "Scott";
+        emp1.job = "Manager";
+        Employee emp2 = new Employee(102, "Dwight"); //constructor 2
+        emp2.job = "Asst. Manager";
+        Employee emp3 = new Employee(103, "Pam", "Clerk"); //constructor 1
+        Employee emp4 = new Employee() { empName = "Ford", job = "Executive"};
+
+        Console.WriteLine(Employee.companyName);
+        Console.WriteLine("\nFirst Employee:");
+        Console.WriteLine(emp1.empID);
+        Console.WriteLine(emp1.empName);
+        Console.WriteLine(emp1.job);
+        Console.WriteLine();
+
+        Console.WriteLine("Second Employee:");
+        Console.WriteLine(emp2.empID);
+        Console.WriteLine(emp2.empName);
+        Console.WriteLine(emp2.job);
+        Console.WriteLine();
+
+        Console.WriteLine("Third Employee:");
+        Console.WriteLine(emp3.empID);
+        Console.WriteLine(emp3.empName);
+        Console.WriteLine(emp3.job);
+        Console.WriteLine();
+    }
+}
